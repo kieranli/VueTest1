b2d67411a26b44cdb6f49ac06c619a58f871540d
kieranli <1010950547@qq.com>
2022-08-11 14:42:13 +0800

    第一个脚手架项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+/*
+  该文件是整个项目的<入口文件>
+*/
+//引入Vue
 import Vue from 'vue'
+//引入App组件，它是所有组件的父组件
 import App from './App.vue'
-
+//关闭Vue的生产提示
 Vue.config.productionTip = false
-
+//创建Vue的实例对象
 new Vue({
+  //完成了这个功能：将App组件放入容器中
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')//.$mount为el配置的另一种写法，绑定容器
